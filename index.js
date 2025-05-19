55b35cfd999e87331e9df5a3ada3204b277d2504
kellyma073 <kma073@ucr.edu>
2025-05-19 03:46:45 -0700

    purple theme

--- index.js
+++ index.js
@@ -19,39 +19,17 @@ $(".cat").click(function() {
     $(".dialogue").text("“My aunt and I found a stray kitten in El Monte and took him in. His name is Monty. He’s a really affectionate and adorable lil guy, but he also scratched me a lot.”")
 });
 
-$(".book").click(function() {
-    $(".dialogue").html('“Pride and Prejudice, aka my favorite book, is a Regency-era social satire about the search for happiness, love, and understanding in a world of inflexible expectations and social rules. I have soooo much to say about it. In fact, read my analysis of the novel <a target="_blank" href="misc/Kelly Ma - Book Analysis_ Pride and Prejudice.pdf">HERE</a> !!”');
+$(".research").click(function() {
+    $(".screen1").html('<div class="topic">Undergraduate Research Assistant</div><div class="thin">University of California, Riverside</div><div class="thin">October 2024 - December 2024</div>');
+    $(".screen2").html('<li>Collaborated with Ph.D. students and Professor Mariam Salloum to research machine learning methods for detecting AI-generated code in introductory computer science courses</li>');
+    $(".screen3").html('<ul><li>Implemented models like Support Vector Machines (SVM) and XGBoost to understand their use in classifying code authenticity based on beginner coding patterns and mistakes</li><li>Learned Natural Language Processing techniques, including tokenization and feature extraction, to explore their potential in analyzing student coding patterns</li><li>Conducted literature reviews and analyzed research papers to assess existing solutions and identify gaps in AI detection for education</li></ul>');
 });
 
-$(".winc").click(function() {
-    $(".screen1").html('<div class="topic">Women in Computing</div><div class="thin">React.js, Next.js, Tailwind CSS, GitHub</div><div class="thin">October 2023 - December 2023</div>');
-    $(".screen2").html('<li>Collaborated with team of 8 developers to construct static site generation Next.js website for displaying club information</li><li><p class="gray-space">Eliminated codebase redundancy by 250 lines</p> by developing map data structure and mapping algorithm that converts club event metadata into a React component</li>');
-    $(".screen3").html('<ul><li>Created <p class="gray-space">reusable components</p>, such as modal and picture frame, to streamline display of event details and images</li><li>Enhanced website functionality by resolving <p class="gray-space">mobile responsiveness</p> issues and implementing prioritized features, attending weekly scrum meetings and using <p class="gray-space">AGILE</p> methodologies for efficient task tracking</li></ul><a target="_blank" href="https://winc.cs.ucr.edu/"><img class="www" src="misc/www.png"/></a><a target="_blank" href="https://github.com/acm-ucr/winc-website"><img class="www" src="contact/gitcat.png"/></a>');
+$(".teacher").click(function() {
+    $(".screen1").html('<div class="topic">Coding Instructor</div><div class="thin">Whizara</div><div class="thin">March 2025 - April 2025</div>');
+    $(".screen2").html('<li>Taught 15 sixth-grade students during a week-long Scratch camp by leading lessons, guiding projects, and supporting understanding of core programming concepts</li><li>Helped students build Asteroid Game using event blocks, variables, and control structures, reinforcing skills through hands-on learning</li>');
+    $(".screen3").html('<ul><li>Encouraged engagement and progress by providing real-time feedback, helping debug code, and celebrating student achievements</li></ul><img class="pic" src="pics/babies.png" style="height: 130px"/>');
 });
-
-$(".hsp").click(function() {
-    $(".screen1").html('<div class="topic">Highlander Space Program</div><div class="thin">React.js, Next.js, Tailwind CSS, GitHub</div><div class="thin">January 2024 - March 2024</div>');
-    $(".screen2").html('<li>Coordinated with team of 9 developers to construct static site generation Next.js website for presenting club information</li><li>Integrated React component to showcase contact information and social media links in the footer of all webpages</li>');
-    $(".screen3").html('<ul><li>Mapped text and images on pages describing the club’s various projects and initiatives</li><li>Enhanced website functionality by resolving <p class="gray-space">mobile responsiveness</p> issues and implementing prioritized features, attending weekly scrum meetings and using <p class="gray-space">AGILE</p> methodologies for efficient task tracking</li></ul><a target="_blank" href="https://hsp.ucrhighlanders.org/"><img class="www" src="misc/www.png"/></a><a target="_blank" href="https://github.com/acm-ucr/hsp-website"><img class="www" src="contact/gitcat.png"/></a>');
-});
-
-$(".qwer").click(function() {
-    $(".screen1").html('<div class="topic">Daily Dose</div><div class="thin">React.js, Next.js, Tailwind CSS, GitHub</div><div class="thin">February 2024</div>');
-    $(".screen2").html('<li>Developed the frontend for a web app alongside 3 teammates that enables users to send time capsules to themselves in the future</li><li>Won <p class="gray-space">Best Beginner</p> and <p class="gray-space">Best Use of MongoDB</p> for Major League Hacking’s QWERHacks @ UCLA</li><li>My first hackathon :)</li>');
-    $(".screen3").html('<img class="team" src="misc/qwerteam.jpeg"/><div class="caption">I couldn’t make it to the closing ceremony, so I’m not in this picture… but look at how adorable my team is!</div><a target="_blank" href="https://devpost.com/software/daily-dose-7ks69p"><img class="www" src="misc/devpost.jpg"/></a><a target="_blank" href="https://github.com/NotALobster/daily-dose"><img class="www" src="contact/gitcat.png"/></a>');
-});
-
-$(".bear").click(function() {
-    $(".screen1").html('<div class="topic">BearCare</div><div class="thin">React.js, Next.js, Tailwind CSS, GitHub</div><div class="thin">April 2024</div>');
-    $(".screen2").html('<li>Implemented the frontend for a web app that simplifies locating local hospitals covered by specific insurance providers during emergencies, improving user access to emergency healthcare</li><li>Created insurance plan selection buttons to streamline filtering, completing the project in 24 hours with 3 teammates for BearHack @ UCR</li>');
-    $(".screen3").html('<img class="team" src="misc/bearteam.jpeg"/><div class="caption">Me, my team, and our project at judging !!</div><a target="_blank" href="https://devpost.com/software/bearcare-18lzch"><img class="www" src="misc/devpost.jpg"/></a><a target="_blank" href="https://github.com/aditithanekar/bearhack"><img class="www" src="contact/gitcat.png"/></a>');
-});
-
-$(".rpg").click(function() {
-    $(".screen1").html('<div class="topic">I Woke Up in a Cave & <br/> All I Want is WiFi</div><div class="thin">C++, GoogleTest, GitHub, Valgrind, GDB, Gcov, Lcov</div><div class="thin">October 2024 - December 2024</div>');
-    $(".screen2").html('<li>Developed a terminal-based fantasy strategy game in a team of 5, implementing core mechanics and design features in C++</li><li>Built a dynamic <p class="gray-space">Inventory</p> system with vector-based storage to streamline item management</li>');
-    $(".screen3").html('<ul><li>Designed and integrated <p class="gray-space">Item</p>, <p class="gray-space">Potion</p>, and <p class="gray-space">Weapon</p> classes, managing item usage and stat modifications to optimize gameplay dynamics</li><li>Validated functionality thru unit testing with <p class="gray-space">GoogleTest</p> and CI pipelines using <p class="gray-space">GitHub Actions</p>, while debugging and profiling with <p class="gray-space">Valgrind</p>, <p class="gray-space">GDB</p>, <p class="gray-space">Gcov</p>, and <p class="gray-space">Lcov</p> to ensure optimal performance</li></ul><a target="_blank" href="https://github.com/kellyma073/rpg-cave-game"><img class="www" src="contact/gitcat.png"/></a>');
- });
 
  var song = new Audio("music/sexypigeon.mp3");
  var isPlaying = false;
